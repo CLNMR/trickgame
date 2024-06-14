8278cab8ede0c21210a98cb82fa974952746b4f6
FBalzerMPE <fbalzer@mpe.mpg.de>
2024-06-14 21:59:14 +0200

    Implemented CardColor.

---
+++ tricking_bees_flutter/packages/tb_core/lib/src/models/card_color.dart
@@ -0,0 +1,28 @@
+/// The possible card colors.
+enum CardColor {
+  /// Red.
+  red,
+
+  /// Green.
+  green,
+
+  /// Blue.
+  blue,
+
+  /// Yellow.
+  yellow,
+
+  /// Black?
+  black;
+
+  /// Retrieve the number of colors for a given number of players.
+  static int getColorNumForPlayerNum(int playerNum) => playerNum == 2
+      ? 3
+      : [3, 4].contains(playerNum)
+          ? 4
+          : 5;
+
+  /// Retrieve the relevant colors for a given number of players.
+  static List<CardColor> getColorsForPlayerNum(int playerNum) =>
+      CardColor.values.sublist(0, getColorNumForPlayerNum(playerNum));
+}
