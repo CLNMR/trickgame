914a0015221e1d5c310ac2faa541f169881de4e3
FBalzerMPE <fbalzer@mpe.mpg.de>
2024-06-21 19:52:47 +0200

    Added hex values and parsing to CardColor

--- tricking_bees_flutter/packages/tb_core/lib/src/cards/card_color.dart
+++ tricking_bees_flutter/packages/tb_core/lib/src/cards/card_color.dart
@@ -1,19 +1,24 @@
 /// The possible card colors.
 enum CardColor {
   /// Red.
-  red,
+  red(0xFFFF0000),
 
   /// Green.
-  green,
+  green(0xFF00FF00),
 
   /// Blue.
-  blue,
+  blue(0xFF0000FF),
 
   /// Yellow.
-  yellow,
+  yellow(0xFFFFFF00),
 
-  /// Black?
-  violet;
+  /// Violet.
+  violet(0xFFFF00FF);
+
+  const CardColor(this.hexValue);
+
+  /// The color's corresponding hex value.
+  final int hexValue;
 
   /// Retrieve the number of colors for a given number of players.
   static int getColorNumForPlayerNum(int playerNum) => playerNum == 2
@@ -21,6 +26,12 @@ enum CardColor {
       : [3, 4].contains(playerNum)
           ? 4
           : 5;
+
+  /// Try to parse a [CardColor] from a string.
+  static CardColor? tryParse(String name) =>
+      (CardColor.values.any((e) => e.toString() == name))
+          ? CardColor.values.firstWhere((color) => color.toString() == name)
+          : null;
 
   /// Retrieve the relevant colors for a given number of players.
   static List<CardColor> getColorsForPlayerNum(int playerNum) =>
