00334986bfd2c84f0bf89a76d8b4afd5f7ed4c2c
Colin Ihlenfeldt <colin.ihlenfeldt@univelop.de>
2025-01-26 21:20:07 +0100

    Fix errors for Flutter 3.27

--- tricking_bees_flutter/packages/tricking_bees/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tricking_bees_flutter/packages/tricking_bees/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,10 +11,11 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import geolocator_apple
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- tricking_bees_flutter/packages/tricking_bees/windows/flutter/generated_plugins.cmake
+++ tricking_bees_flutter/packages/tricking_bees/windows/flutter/generated_plugins.cmake
@@ -9,6 +9,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   firebase_storage
+  geolocator_windows
+  permission_handler_windows
   share_plus
   url_launcher_windows
 )
